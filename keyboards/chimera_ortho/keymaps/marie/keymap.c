3ddf1675ffbbbb7c030ba9c7025415056472fa4b
sharctgx <sharctgx@yandex.ru>
2021-04-10 14:51:58 +0300

    different improvements

--- keyboards/chimera_ortho/keymaps/marie/keymap.c
+++ keyboards/chimera_ortho/keymaps/marie/keymap.c
@@ -1,3 +1,4 @@
+
 // this is the style you want to emulate.
 // This is the canonical layout file for the Quantum project. If you want to add another keyboard,
 
@@ -25,6 +26,7 @@ enum chimera_ortho_layers {
 #define KC_NTAB LCTL(KC_PGDN)
 #define KC_LDSK LCTL(LALT(KC_LEFT))
 #define KC_RDSK LCTL(LALT(KC_RGHT))
+#define KC_MDSH UC(0x2014)
 
 // Set a default OS
 void matrix_init_user(){
@@ -35,35 +37,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
   //,-------+-------+-------+-------+-------+-------+-------.     ,-------+-------+-------+-------+-------+-------+-------.
-    KC_GRAVE, KC_Q  , KC_W  , KC_E  , KC_R  , KC_T ,UC(0x2014),   KC_CAPS, KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_LBRC,
+    KC_GRAVE, KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,KC_NLCK,      KC_CAPS, KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_LBRC,
   //|-------+-------+-------+-------+-------+-------+-------|     |-------+-------+-------+-------+-------+-------+-------|
-     KC_TAB , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,KC_ENT ,      KC_DEL , KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,LGUI_C,
+     KC_TAB , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,KC_ENT ,      KC_DEL , KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN, LGUI_C,
   //|-------+-------+-------+-------+-------+-------+-------|     |-------+-------+-------+-------+-------+-------+-------|
-     LCTL_C , KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,KC_LAPO,      KC_RAPC, KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH,RCTL_C ,
+     LCTL_C , KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,KC_RALT,      KC_LALT, KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_SLSH, RCTL_C,
   //|-------+-------+-------+-------+-------+-------+-------|     |-------+-------+-------+-------+-------+-------+-------|
-                                  KC_ESC_NAV,KC_BS_SHIFT,                  KC_SP_SHIFT,KC_ENTER_NUM
+                                  KC_ESC_NAV,KC_SP_SHIFT,                KC_BS_SHIFT,KC_ENTER_NUM
   // \------------------------------+-------+-------+------/       \------+-------+-------+------------------------------/
   ),
 
   [_NUMPAD_SYMBOLS] = LAYOUT(
   //,-------+-------+-------+-------+-------+-------+-------.     ,-------+-------+-------+-------+-------+-------+-------.
-     _______,KC_PSLS,KC_7   ,KC_8   ,KC_9   ,KC_PPLS,_______,      _______,_______,KC_CIRC,KC_BSLS,KC_AT  ,KC_PERC,_______,
+     _______,KC_PSLS,KC_P7  ,KC_P8  ,KC_P9  ,KC_PPLS,_______,      _______,_______,KC_HOME,KC_END ,KC_PTAB,KC_NTAB,_______,
   //|-------+-------+-------+-------+-------+-------+-------|     |-------+-------+-------+-------+-------+-------+-------|
-     _______,KC_PAST,KC_4   ,KC_5   ,KC_6   ,KC_PMNS,_______,      _______,_______,KC_UNDS,KC_EXLM,KC_HASH,KC_AMPR,_______,
+     _______,KC_PAST,KC_P4  ,KC_P5  ,KC_P6  ,KC_PMNS,_______,      _______,KC_LEFT,KC_DOWN, KC_UP ,KC_RGHT,KC_INS ,_______,
   //|-------+-------+-------+-------+-------+-------+-------|     |-------+-------+-------+-------+-------+-------+-------|
-     _______,KC_PEQL,KC_1   ,KC_2   ,KC_3   ,KC_PDOT,_______,      _______,_______,KC_COLN,KC_DLR ,KC_PIPE,_______,_______,
+     _______,KC_COLN,KC_P1  ,KC_P2  ,KC_P3  ,KC_EQL ,_______,      _______,_______,KC_LDSK,KC_RDSK,KC_PSCR,KC_CAD ,_______,
   //|-------+-------+-------+-------+-------+-------+-------|     |-------+-------+-------+-------+-------+-------+-------|
-                                     KC_0   ,_______,                      _______,_______
+                                     KC_P0  ,_______,                      _______,_______
   // \------------------------------+-------+-------+------/       \------+-------+-------+------------------------------/
-  ),
-
-  [_NAV] = LAYOUT(
+  ),                                                                       // we can put more symbols here if needed
+                                                                           // |
+  [_NAV] = LAYOUT(                                                         // V
   //,-------+-------+-------+-------+-------+-------+-------.     ,-------+-------+-------+-------+-------+-------+-------.
-     KC_VOLU,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_BRIU,_______,      _______,KC_HOME,KC_END ,KC_PTAB,KC_NTAB,KC_PGUP,_______,
+     KC_VOLU,KC_F9  ,KC_F10 ,KC_F11 ,KC_F12 ,KC_BRIU,_______,      _______,KC_MDSH,KC_UNDS,KC_AMPR,KC_PIPE,KC_CIRC,_______,
   //|-------+-------+-------+-------+-------+-------+-------|     |-------+-------+-------+-------+-------+-------+-------|
-     KC_VOLD,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8  ,KC_BRID,_______,      _______,KC_LEFT,KC_DOWN, KC_UP ,KC_RGHT,KC_PGDN,_______,
+     KC_VOLD,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8  ,KC_BRID,_______,      _______,KC_BSLS,KC_EXLM,KC_EQL, KC_AT  ,KC_PERC,_______,
   //|-------+-------+-------+-------+-------+-------+-------|     |-------+-------+-------+-------+-------+-------+-------|
-     KC_MUTE,KC_F1,  KC_F2  ,KC_F3  ,KC_F4  ,_______,_______,      _______,KC_INS ,KC_PSCR,KC_LDSK,KC_RDSK,KC_CAD ,_______,
+     KC_MUTE,KC_F1,  KC_F2  ,KC_F3  ,KC_F4  ,_______,_______,      _______,_______,KC_LPRN,KC_RPRN,KC_DLR ,KC_HASH,_______,
   //|-------+-------+-------+-------+-------+-------+-------|     |-------+-------+-------+-------+-------+-------+-------|
                                      _______,_______,                      _______,_______
   // \------------------------------+-------+-------+------/       \------+-------+-------+------------------------------/
@@ -71,9 +73,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 // Идеи:
-// 2) em-dash
-// 3) совместить по максимуму спец. символы для двух раскладок (поместить их в одно место, но под разные модификаторы)
-// 4) есть проблема с тем, что нельзя нажимать стрелки одной рукой((
+// 3) протестить спец. символы для двух раскладок
+// 5) сделать возможность залипания слоев
+
 
 void matrix_scan_user(void) {
     uint8_t layer = biton32(layer_state);
